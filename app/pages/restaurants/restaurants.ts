11e7efb1fa2dd2b05205038fc83e0f50c7e5bc17
rlmcneary2 <rlmcneary2@gmail.com>
2023-09-07 01:15:21 -0400

    Add API for regions and cities.

--- app/pages/restaurants/restaurants.ts
+++ app/pages/restaurants/restaurants.ts
@@ -1,11 +1,16 @@
 export class Restaurants extends HTMLElement {
   private _connected = false;
+  private _regionsLock: Promise<void> | undefined;
+  protected _cities: { [region: string]: [{ name: string }] } | undefined;
+  protected _regions: { name: string; short: string }[] | undefined;
   protected _shadowRoot: ShadowRoot;
 
   constructor() {
     super();
 
     this._shadowRoot = this.attachShadow({ mode: "closed" });
+
+    this.getRegions();
   }
 
   static get webComponentName() {
@@ -23,6 +28,14 @@ export class Restaurants extends HTMLElement {
     link.href = "/app/place-my-order-assets.css";
     link.rel = "stylesheet";
 
+    const content = this.getContent();
+
+    this.populateRegionsList(content);
+
+    this._shadowRoot.append(link, content);
+  }
+
+  protected getContent() {
     //   <div className="restaurants">
     //     <h2 className="page-header">Restaurants</h2>
     //     <form className="form">
@@ -49,20 +62,114 @@ export class Restaurants extends HTMLElement {
     <form class="form">
       <div class="form-group">
         <label>State</label>
-        <select>
+        <select id="region">
           <option value="">Choose a state</option>
         </select>
       </div>
       <div class="form-group">
         <label>City</label>
-        <select>
+        <select id="city">
           <option value="">Choose a city</option>
         </select>
       </div>
     </form>
   </div>`;
 
-    this._shadowRoot.append(link, div);
+    const region = div.querySelector("#region") as HTMLSelectElement;
+    region.addEventListener("change", evt => {
+      this.populateCitiesList((evt.target as HTMLSelectElement).value, div);
+    });
+
+    const city = div.querySelector("#city") as HTMLSelectElement;
+    city.addEventListener("change", () =>
+      console.log("Restaurants.connectedCallback: city change.")
+    );
+
+    return div;
+  }
+
+  protected async populateCitiesList(region: string, content: HTMLElement) {
+    const cities = await this.getCities(region);
+
+    if (!cities) {
+      return;
+    }
+
+    const select = content.querySelector("#city") as HTMLSelectElement;
+    if (!select) {
+      return;
+    }
+
+    select.innerHTML = "";
+
+    select.append(
+      ...cities.map(({ name }) => {
+        const opt = document.createElement("option");
+        opt.value = name;
+        opt.textContent = name;
+        return opt;
+      })
+    );
+  }
+
+  protected async populateRegionsList(content: HTMLElement) {
+    await this.getRegions();
+
+    if (!this._regions) {
+      return;
+    }
+
+    const region = content.querySelector("#region") as HTMLSelectElement;
+    if (!region) {
+      return;
+    }
+
+    region.append(
+      ...this._regions.map(({ name, short }) => {
+        const opt = document.createElement("option");
+        opt.value = short;
+        opt.textContent = name;
+        return opt;
+      })
+    );
+  }
+
+  private getCities(region: string): Promise<{ name: string }[] | undefined> {
+    if (this._cities) {
+      return Promise.resolve(this._cities[region]);
+    }
+
+    return new Promise<{ name: string }[] | undefined>(resolve => {
+      this.apiFetch("app/api/cities.json", response =>
+        response.json().then(body => {
+          this._cities = body.data;
+          resolve(this._cities ? this._cities[region] : undefined);
+        })
+      );
+    });
+  }
+
+  private getRegions(): Promise<void> {
+    if (!this._regionsLock) {
+      this._regionsLock = this.apiFetch("app/api/regions.json", response =>
+        response.json().then(body => {
+          this._regions = body.data;
+        })
+      );
+    }
+
+    return this._regionsLock;
+  }
+
+  private apiFetch(
+    path: string,
+    callback: (response: Response) => Promise<void>
+  ): Promise<void> {
+    return new Promise<void>(resolve => {
+      fetch(path)
+        .then(response => callback(response))
+        .then(() => resolve());
+    });
   }
 }
 
