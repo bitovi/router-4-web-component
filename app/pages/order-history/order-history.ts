28b7d417ec594dd1ce81b6572e7d617dbcfbecce
rlmcneary2 <rlmcneary2@gmail.com>
2023-09-10 14:30:41 -0400

    Add order history page.

---
+++ app/pages/order-history/order-history.ts
@@ -0,0 +1,26 @@
+import { Basecomp } from "../../components/basecomp/basecomp.ts";
+
+export class OrderHistory extends Basecomp(HTMLElement) {
+  #shadowRoot: ShadowRoot;
+
+  constructor() {
+    super();
+
+    this.#shadowRoot = this.attachShadow({ mode: "closed" });
+  }
+
+  static get webComponentName() {
+    return "app-order-history";
+  }
+
+  override componentConnected(): void {
+    const html = `<link href="/app/assets/place-my-order-assets.css" rel="stylesheet"></link>
+<div class="order-history"><h1>order history</h1></div>`;
+
+    this.#shadowRoot.innerHTML = html;
+  }
+}
+
+if (!customElements.get(OrderHistory.webComponentName)) {
+  customElements.define(OrderHistory.webComponentName, OrderHistory);
+}
