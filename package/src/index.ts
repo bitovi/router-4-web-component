635395c970c140a5774d5ef64dd82e9cc313c305
rlmcneary2 <rlmcneary2@gmail.com>
2023-09-21 22:23:06 -0400

    Added the TemplateMixin to fetch HTML files that are templates.

--- package/src/index.ts
+++ package/src/index.ts
@@ -5,6 +5,7 @@ import { ParamsListenerMixin } from "./mixins/params-listener/params-listener.ts
 import { Route } from "./components/route/route.ts";
 import { Router } from "./components/router/router.ts";
 import { Switch } from "./components/switch/switch.ts";
+import { TemplateMixin } from "./mixins/template/template.ts";
 
 export {
   ComponentLifecycleMixin,
@@ -13,5 +14,6 @@ export {
   ParamsListenerMixin,
   Route,
   Router,
-  Switch
+  Switch,
+  TemplateMixin
 };
